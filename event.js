e4a479aede8a17c7847b54f1be0ae4b71ac79edd
ConnorCoder <43045469+ConnorCoder@users.noreply.github.com>
2019-03-12 16:52:38 -0400

    Update event.js

--- event.js
+++ event.js
@@ -1,5 +1,5 @@
 
-var game = {points: 0, pointspc: 1, chance: 100}; //Default Stats
+var game = {points: 0, pointspc: 1, chance: 100, pointsps: 0}; //Default Stats
 
 if (localStorage.getItem('points') === null) { //Load Game
 	localStorage.setItem('points', 0);
@@ -16,17 +16,17 @@ if (localStorage.getItem('chance') === null) {
 }else {
 	game.chance = parseInt(localStorage.getItem('chance'));
 }
+if (localStorage.getitem('pointsps') === null) {
+	localStorage.setItem('pointsps', 0);
+}else {
+	game.pointsps = localStorage.getItem('pointsps');
+}
 function reset() {
 	game.points = 0;
 	game.pointspc = 1;
 	game.chance = 100;
+	game.pointsps = 0;
 }
-function savegame() { //Force Save the Game
-	localStorage.setItem('points', game.points);
-	localStorage.setItem('pointspc', game.pointspc);
-	localStorage.setItem('chance', game.chance);
-}
-
 function btnclick() { //Function ran when button is clicked
 	var rand = Math.floor(Math.random() * game.chance);
 	var btn = document.getElementById("game-btn");
@@ -38,31 +38,38 @@ function btnclick() { //Function ran when button is clicked
   		btn.style.top = random2 + "%";
 	}
 }
-function upgrade(x) { //Upgrade your PPS (points per second)
+function upgrade(x) { //All Upgrades/Purchases
 	if (x < 19) {
 		if (game.points >= Math.pow(10, x)) {
 			game.pointspc += Math.pow(10, x - 2);
 			game.points -= Math.pow(10, x);
 		}
 	}else if (x < 22) {
-      if (game.points >= Math.pow(10, x - 13)) {
-        game.chance -= Math.pow(10, x - 19);
-        game.points -= Math.pow(10, x - 13);
-      }
-    }
+      		if (game.points >= Math.pow(10, x - 13)) {
+        		game.chance -= Math.pow(10, x - 19);
+        		game.points -= Math.pow(10, x - 13);
+      		}
+    	}else if (x < 31) {
+		if (game.points >= Math.pow(10, x - 19)) {
+			game.pointsps += Math.pow(10, x - 22);
+			game.points -= Math.pow(10, x - 19);
+		}
+	}
 }
 
 function hideall() {
 	document.getElementById("pointspc").style.display = "none";
 	document.getElementById("chance").style.display = "none";
+	document.getElementById("pointsps").style.display = "none";
 }
 
 function repeat() { //Save the game and display the last save
 	var x = document.getElementById("game-localstorage");
 	savegame();
-	x.innerHTML = "Last Save: Points = " + localStorage.getItem("points") + ", PointsPc = " + localStorage.getItem("pointspc") + ", Chance = " + localStorage.getItem("chance");
+	x.innerHTML = "Last Save: Points = " + localStorage.getItem("points") + ", PointsPc = " + localStorage.getItem("pointspc") + ", Chance = " + localStorage.getItem("chance") + ", PointsPs = " + localStorage.getitem("pointsps");
 	document.getElementById("game-pointspc").innerHTML = "Points Per Click: " + game.pointspc;
 	document.getElementById("game-points").innerHTML = "Points: " + game.points;
+	document.getElementById("game-pointsps").innerHTML = "Points Per Second: " + game.pointsps;
 	var opt = document.getElementById("select").value;
 	if (opt == 0) {
 		hideall();
@@ -70,6 +77,14 @@ function repeat() { //Save the game and display the last save
 	}else if (opt == 1) {
 		hideall();
 		document.getElementById("chance").style.display = "block";
+	}else if (opt == 2) {
+		hideall();
+		document.getElementById("pointsps").style.display = "block";
 	}
 }
 setInterval(repeat, 0);
+
+function pps() {
+	game.points += game.pointsps;
+}
+setInterval(pps, 1000);
